82a327455cf675e0ca4c4aa6394737ee142f29bc
Matheus Pereira <matheus@stargrid.pro>
2024-06-19 13:04:12 -0300

    UserEntity - getters method unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -16,9 +16,32 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Constructor method', () => {
-    expect(sut.props.name).toEqual(props.name)
-    expect(sut.props.email).toEqual(props.email)
-    expect(sut.props.password).toEqual(props.password)
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.name).toEqual(props.name)
+    expect(sut.email).toEqual(props.email)
+    expect(sut.password).toEqual(props.password)
+    expect(sut.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('Getter of name field', () => {
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
+  })
+
+  it('Getter of email field', () => {
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe('string')
+  })
+
+  it('Getter of password field', () => {
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe('string')
+  })
+
+  it('Getter of createdAt field', () => {
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 })
