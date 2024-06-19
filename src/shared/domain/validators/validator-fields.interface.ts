3aa6a8f68ed08c44a63c50ef0ece53d2e6335e60
Matheus Pereira <matheus@stargrid.pro>
2024-06-19 18:26:15 -0300

    Adding interface ValidatorFieldsInterface

---
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -0,0 +1,9 @@
+export type FieldErrors = {
+  [field: string]: string[]
+}
+
+export interface ValidatorFieldsInterface<PropsValidated> {
+  errors: FieldErrors
+  validatedData: PropsValidated
+  validate(data: any): boolean
+}
