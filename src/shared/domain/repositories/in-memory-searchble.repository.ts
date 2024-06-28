848b0385f5384545f01fd207d8d277cee97d9203
Matheus Pereira <matheus@stargrid.pro>
2024-06-28 12:11:33 -0300

    Adding search method in InMemorySearchbleRepository class

--- src/shared/domain/repositories/in-memory-searchble.repository.ts
+++ src/shared/domain/repositories/in-memory-searchble.repository.ts
@@ -1,12 +1,33 @@
 import { Entity } from '../entities/entity'
 import { InMemoryRepository } from './in-memory.repository'
-import { SearchbleRepositoryInterface } from './searchable-repository-contracts'
+import {
+  SearchbleRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contracts'
 
 export abstract class InMemorySearchRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchbleRepositoryInterface<E, any, any>
 {
-  search(props: any): Promise<any> {
+  async search(props: SearchParams): Promise<SearchResult<E>> {
     throw new Error('Method not implemented.')
   }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'] | null,
+    perPage: SearchParams['perPage'] | null,
+  ): Promise<E[]> {}
 }
